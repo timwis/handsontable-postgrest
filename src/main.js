7df9620184a3391ab9c55e30baeac9c592be4394
timwis <tim@timwis.com>
2015-12-27 22:22:09 -0500

    lose ampersand

--- src/main.js
+++ src/main.js
@@ -1,38 +1,83 @@
-var PostgRESTModel = require('./models/postgrest-model')
-var PostgRESTCollection = require('./collections/postgrest-collection')
-var HandsontableMixin = require('./collections/handsontable-mixin')
-var Schema = require('./collections/postgrest-schema')
-var Grid = require('./views/grid')
-var _ = {clone: require('lodash/lang/clone')}
+var request = require('then-request')
+var Promise = require('promise')
+var Handsontable = require('handsontable')
+var _ = { pluck: require('lodash/collection/pluck') }
 
-// Mix in missing array methods necessary for handsontable
-var GridCollection = PostgRESTCollection.extend(HandsontableMixin)
+var container = document.querySelector('#grid')
+var baseUrl = 'http://postgrest.herokuapp.com'
+var table = 'speakers'
 
-var schema = new Schema(null, {table: 'candidates'})
-schema.fetch({
-  success: function (schemaCollection, response, options) {
-    // Reduce schemaCollection to a props hash for the Ampersand model
-    var props = schemaCollection.reduce(function (result, model) {
-      result[model.name] = model.propType
-      return result
-    }, {})
+// Get schema
+Promise.all([
+  request('OPTIONS', baseUrl + '/' + table),
+  request('GET', baseUrl + '/' + table)
+])
+.then(function (responses) {
+  var schema = JSON.parse(responses[0].getBody())
+  var rows = JSON.parse(responses[1].getBody())
+  var primaryKey = schema.pkey[0]
+  var headers = _.pluck(schema.columns, 'name')
+  var columns = headers.map(function (header) {
+    return {
+      data: header,
+      readOnly: header === primaryKey
+    }
+  })
 
-    // Create a model for this table using the schema obtained
-    var GridModel = PostgRESTModel.extend({
-      props: props,
-      idAttribute: schemaCollection.primaryKey
-    })
+  Handsontable(container, {
+    data: rows,
+    columns: columns,
+    colHeaders: headers,
+    contextMenu: true,
+    minSpareRows: 1,
+    afterChange: function (changes, source) {
+      if (['edit', 'empty', 'autofill', 'paste'].indexOf(source) !== -1) {
+        changes.forEach(function (change) {
+          var rowIndex = change[0]
+          var identifier = this.getDataAtRowProp(rowIndex, primaryKey)
+          var property = change[1]
+          var newValue = change[3]
 
-    // Initialize a collection using our new model
-    var gridCollection = new GridCollection(null, {
-      model: GridModel,
-      table: 'candidates'
-    })
-    gridCollection.fetch({
-      success: function (collection, response, options) {
-        var container = document.querySelector('#grid')
-        new Grid(container, schemaCollection, collection) // eslint-disable-line
+          var changes = {}
+          changes[property] = newValue
+
+          if (identifier) {
+            var qs = {}
+            qs[primaryKey] = 'eq.' + identifier
+
+            request('PATCH', baseUrl + '/' + table, {
+              qs: qs,
+              json: changes
+            })
+          } else {
+            var context = this
+            request('POST', baseUrl + '/' + table, {
+              json: changes
+            })
+            .then(function (createRecordResponse) {
+              request('GET', baseUrl + createRecordResponse.headers.location)
+              .then(function (getNewRecordResponse) {
+                var newData = JSON.parse(getNewRecordResponse.getBody())[0]
+                for (var key in newData) {
+                  context.setDataAtRowProp(rowIndex, key, newData[key], 'loadData')
+                }
+              })
+            })
+          }
+        }, this)
       }
-    })
-  }
+    },
+    beforeRemoveRow: function (index, amount, logicRows) {
+      console.log('removing', logicRows)
+      logicRows.forEach(function (row) {
+        var identifier = this.getDataAtRowProp(row, primaryKey)
+
+        var qs = {}
+        qs[primaryKey] = 'eq.' + identifier
+        request('DELETE', baseUrl + '/' + table, {
+          qs: qs
+        })
+      }, this)
+    }
+  })
 })
