477c8130d9981d1d32b279ffaab1871d3aad7d66
timwis <tim@timwis.com>
2015-12-31 17:09:36 -0500

    render name param on foreign entities

--- src/main.js
+++ src/main.js
@@ -12,25 +12,23 @@ var _ = {
 
 var container = document.querySelector('#grid')
 var baseUrl = 'http://phlcrud.herokuapp.com'
-var table = 'candidates'
+var table = 'candidates' // SHOULD NOT BE HARD-CODED
 
 // Get schema and rows
 NProgress.start()
-Promise.all([
-  request('OPTIONS', baseUrl + '/' + table),
-  request('GET', baseUrl + '/' + table)
-])
-.then(function (responses) {
-  NProgress.done()
-  var schema = JSON.parse(responses[0].getBody())
-  var rows = JSON.parse(responses[1].getBody())
+request('OPTIONS', baseUrl + '/' + table)
+.then(function (schemaResponse) {
+  var schema = JSON.parse(schemaResponse.getBody())
+  var selectParam = constructSelectParam(schema)
   var primaryKey = schema.pkey[0]
   var headers = _.pluck(schema.columns, 'name')
+
   var columns = schema.columns.map(function (column) {
     var columnConfig = {
-      data: column.name,
+      data: column.references ? column.name + '.name' : column.name, // SHOULD NOT BE HARD-CODED
       readOnly: column.name === primaryKey
     }
+    // Editor
     if (column.references) {
       columnConfig.editor = 'select'
       columnConfig.source = function (query, callback) { // returns promise
@@ -38,7 +36,7 @@ Promise.all([
         .then(function (response) {
           var rows = JSON.parse(response.getBody())
           return rows.reduce(function (hash, item) {
-            hash[item[column.references.column]] = item.name // HARD-CODED
+            hash[item[column.references.column]] = item.name // SHOULD NOT BE HARD-CODED
             return hash
           }, {})
         })
@@ -47,100 +45,108 @@ Promise.all([
     return columnConfig
   })
 
-  Handsontable(container, {
-    data: rows,
-    columns: columns,
-    colHeaders: headers,
-    contextMenu: true,
-    minSpareRows: 1,
-    undo: true,
-    columnSorting: true,
-    sortIndicator: true,
-    persistentState: true,
-    afterChange: function (changes, source) {
-      var context = this
-      // Only listen to changes from certain sources
-      if (['edit', 'empty', 'autofill', 'paste', 'undo', 'redo'].indexOf(source) === -1) {
-        return
-      }
-      // Group changes by row and construct a hash of each row's changes
-      var changesByRow = {}
-      changes.forEach(function (change) {
-        var rowIndex = change[0]
-        var property = change[1]
-        var newValue = change[3]
+  request('GET', baseUrl + '/' + table, {
+    qs: selectParam ? {select: selectParam} : null
+  })
+  .then(function (rowsResponse) {
+    NProgress.done()
+    var rows = JSON.parse(rowsResponse.getBody())
 
-        if (!changesByRow[rowIndex]) changesByRow[rowIndex] = {}
-        changesByRow[rowIndex][property] = newValue
+    Handsontable(container, {
+      data: rows,
+      columns: columns,
+      colHeaders: headers,
+      contextMenu: true,
+      minSpareRows: 1,
+      undo: true,
+      columnSorting: true,
+      sortIndicator: true,
+      persistentState: true,
+      afterChange: function (changes, source) {
+        var context = this
+        // Only listen to changes from certain sources
+        if (['edit', 'empty', 'autofill', 'paste', 'undo', 'redo'].indexOf(source) === -1) {
+          return
+        }
+        // Group changes by row and construct a hash of each row's changes
+        var changesByRow = {}
+        changes.forEach(function (change) {
+          var rowIndex = change[0]
+          var property = change[1]
+          var newValue = change[3]
 
-        // Show loading indicator on cell
-        var colIndex = this.propToCol(property)
-        this.getCell(rowIndex, colIndex).classList.toggle('syncing', true)
-      }, this)
+          if (!changesByRow[rowIndex]) changesByRow[rowIndex] = {}
+          changesByRow[rowIndex][property] = newValue
 
-      // Send a request for each row that's changed
-      for (var rowIndex in changesByRow) {
-        var rowChanges = changesByRow[rowIndex]
-        var identifier = this.getDataAtRowProp(rowIndex, primaryKey)
+          // Show loading indicator on cell
+          var colIndex = this.propToCol(property)
+          this.getCell(rowIndex, colIndex).classList.toggle('syncing', true)
+        }, this)
 
-        if (identifier) {
-          // If there's an identifier already, edit the record
+        // Send a request for each row that's changed
+        for (var rowIndex in changesByRow) {
+          var rowChanges = changesByRow[rowIndex]
+          var identifier = this.getDataAtRowProp(rowIndex, primaryKey)
+
+          if (identifier) {
+            // If there's an identifier already, edit the record
+            var qs = {}
+            qs[primaryKey] = 'eq.' + identifier
+
+            setPendingRequests(1)
+            request('PATCH', baseUrl + '/' + table, {
+              qs: qs,
+              json: rowChanges
+            })
+            .then(function (scopedRowIndex) { // necessary because rowIndex gets changed by the for..in loop
+              return function () {
+                setPendingRequests(-1)
+
+                // Remove loading indicator from every cell in this row (NodeLists are fun!....)
+                var syncingCells = context.getCell(scopedRowIndex, 0).parentNode.querySelectorAll('.syncing')
+                for (var i = 0; i < syncingCells.length; i++) {
+                  syncingCells[i].classList.toggle('syncing', false)
+                }
+              }
+            }(rowIndex))
+          } else {
+            // If there's no identifier, create the record
+            setPendingRequests(1)
+            request('POST', baseUrl + '/' + table, {
+              json: rowChanges,
+              headers: {Prefer: 'return=representation'}  // return the new record
+            })
+            .then(function (scopedRowIndex) {
+              return function (createRecordResponse) {
+                setPendingRequests(-1)
+
+                // Set the data in the table based on the new record's data (ex. auto generated ID)
+                var newData = JSON.parse(createRecordResponse.getBody())
+                for (var key in newData) {
+                  context.setDataAtRowProp(scopedRowIndex, key, newData[key], 'loadData')
+                }
+              }
+            }(rowIndex))
+          }
+        }
+      },
+      beforeRemoveRow: function (index, amount, logicRows) {
+        for (var rowIndex = index; rowIndex < index + amount; rowIndex++) {
+          var identifier = this.getDataAtRowProp(rowIndex, primaryKey)
+
           var qs = {}
           qs[primaryKey] = 'eq.' + identifier
 
           setPendingRequests(1)
-          request('PATCH', baseUrl + '/' + table, {
-            qs: qs,
-            json: rowChanges
+          request('DELETE', baseUrl + '/' + table, {
+            qs: qs
           })
-          .then(function (scopedRowIndex) { // necessary because rowIndex gets changed by the for..in loop
-            return function () {
-              setPendingRequests(-1)
-
-              // Remove loading indicator from every cell in this row (NodeLists are fun!....)
-              var syncingCells = context.getCell(scopedRowIndex, 0).parentNode.querySelectorAll('.syncing')
-              for (var i = 0; i < syncingCells.length; i++) {
-                syncingCells[i].classList.toggle('syncing', false)
-              }
-            }
-          }(rowIndex))
-        } else {
-          // If there's no identifier, create the record
-          setPendingRequests(1)
-          request('POST', baseUrl + '/' + table, {
-            json: rowChanges,
-            headers: {Prefer: 'return=representation'}  // return the new record
+          .then(function () {
+            setPendingRequests(-1)
           })
-          .then(function (scopedRowIndex) {
-            return function (createRecordResponse) {
-              setPendingRequests(-1)
-
-              // Set the data in the table based on the new record's data (ex. auto generated ID)
-              var newData = JSON.parse(createRecordResponse.getBody())
-              for (var key in newData) {
-                context.setDataAtRowProp(scopedRowIndex, key, newData[key], 'loadData')
-              }
-            }
-          }(rowIndex))
         }
       }
-    },
-    beforeRemoveRow: function (index, amount, logicRows) {
-      for (var rowIndex = index; rowIndex < index + amount; rowIndex++) {
-        var identifier = this.getDataAtRowProp(rowIndex, primaryKey)
-
-        var qs = {}
-        qs[primaryKey] = 'eq.' + identifier
-
-        setPendingRequests(1)
-        request('DELETE', baseUrl + '/' + table, {
-          qs: qs
-        })
-        .then(function () {
-          setPendingRequests(-1)
-        })
-      }
-    }
+    })
   })
 })
 
@@ -153,5 +159,18 @@ var setPendingRequests = function (increment) {
     window.onbeforeunload = function () {
       return "There are changes that haven't been saved yet"
     }
+  }
+}
+
+var constructSelectParam = function (schema) {
+  var foreignEntities = []
+  schema.columns.forEach(function (column) {
+    if (column.references) foreignEntities.push(column.name + '{*}')
+  })
+  if (foreignEntities.length) {
+    foreignEntities.unshift('*')
+    return foreignEntities.join(',')
+  } else {
+    return ''
   }
 }
